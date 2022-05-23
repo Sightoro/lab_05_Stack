f5601995c2c1129dd57fce47975ccd08a6868cb7
Sightoro <yuralon.skiy@mail.ru>
2022-05-23 12:53:30 +0300

    try

--- tests/example.cpp
+++ tests/example.cpp
@@ -10,22 +10,18 @@
 TEST(method_tests, r_push) {
   Stack<int> stack;
   stack.push(7);
-  EXPECT_EQ(stack.top_node->data, 7);
-}
+  EXPECT_EQ(stack.top_node->data, 7);}
 
 TEST(method_tests, l_push) {
   Stack<int> stack;
   int a = 1;
   stack.push(a);
-  EXPECT_EQ(stack.top_node->data, 1);
-}
+  EXPECT_EQ(stack.top_node->data, 1);}
 TEST(exeption_tests, empty_stack) {
   Stack<int> stack;
-  EXPECT_ANY_THROW(stack.pop( ));
-}
+  EXPECT_ANY_THROW(stack.pop( ));}
 
 TEST(method_tests, g_pop) {
   Stack<int> stack;
   stack.push(7);
-  EXPECT_EQ(stack.pop( ), 7);
-}
+  EXPECT_EQ(stack.pop( ), 7);}
